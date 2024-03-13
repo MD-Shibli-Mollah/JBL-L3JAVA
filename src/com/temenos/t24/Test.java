f35ba74ef45d2dd45d08a567920544634e45711f
MD SHIBLI MOLLAH <md.ta.shibli@gmail.com>
2024-03-13 15:35:41 +0600

    IRD- SMS Java Codes

--- src/com/temenos/t24/Test.java
+++ src/com/temenos/t24/Test.java
@@ -12,10 +12,20 @@ public class Test {
      * @param args
      */
     public static void main(String[] args) {
-        String myName = "MD TANVIR AHMED SHIBLI MOLLAH SON OF MD SADEK ALI MOLLAH AND LOVELY AK";
+        /*String myName = "MD TANVIR AHMED SHIBLI MOLLAH SON OF MD SADEK ALI MOLLAH AND LOVELY AK";
         Integer myNameLen = myName.length();
         System.out.println(myNameLen);
-        if (myNameLen > 35){
+                
+        System.out.println(myName);*/
+        String dbtAmt = "USD4500";
+        String yDbtAmt = "";
+        yDbtAmt = dbtAmt.substring(3);
+        System.out.println(yDbtAmt);
+        Double dblTxnAmt = 0.0;
+        dblTxnAmt = Double.valueOf(yDbtAmt);
+        System.out.println(dblTxnAmt);
+        
+        /*if (myNameLen > 35){
             String myName35 = myName.substring(0, 35); //Total 35 characters excluding 35th number.
             String myName36 = myName.substring(35, myNameLen);
             
@@ -25,7 +35,7 @@ public class Test {
         else
         {
             System.out.println(myName);
-        }
+        }*/
         
     }
 
