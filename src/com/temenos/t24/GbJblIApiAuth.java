bd332c330bc12c60a751c76f3d26ec0fe5a6cf9e
MD SHIBLI MOLLAH <md.ta.shibli@gmail.com>
2024-04-25 16:20:10 +0600

    API Auth Table Update FIX

--- src/com/temenos/t24/GbJblIApiAuth.java
+++ src/com/temenos/t24/GbJblIApiAuth.java
@@ -37,7 +37,7 @@ public class GbJblIApiAuth extends RecordLifecycle {
         // Encryption key (must be 16 characters for AES encryption)
         // String encryptionKey = "ThisIsASecretKey!";
         String encryptionKey = "MyKey$forJBLApi&!";
-        encryptionKey.getBytes(StandardCharsets.UTF_8);
+        encryptionKey = new String(encryptionKey.getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
 
         // Encrypt username and password
         try {
