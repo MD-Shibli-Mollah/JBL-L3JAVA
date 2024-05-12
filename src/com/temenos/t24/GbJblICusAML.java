256cd5f42f6552abaa44ab144e7783a4bf9b012e
MD SHIBLI MOLLAH <md.ta.shibli@gmail.com>
2024-05-12 17:56:46 +0600

    AML Check Routine - SLEEP removed

--- src/com/temenos/t24/GbJblICusAML.java
+++ src/com/temenos/t24/GbJblICusAML.java
@@ -41,116 +41,114 @@ public class GbJblICusAML extends RecordLifecycle {
     @Override
     public void defaultFieldValues(String application, String currentRecordId, TStructure currentRecord,
             TStructure unauthorisedRecord, TStructure liveRecord, TransactionContext transactionContext) {
-
-        CustomerRecord recordForCustomer = new CustomerRecord(currentRecord);
-
-        String id = "";
-        id = "AML";
-        DataAccess da = new DataAccess(this);
-        EbJblApiAuthTableRecord apiAuthRec = new EbJblApiAuthTableRecord(da.getRecord("EB.JBL.API.AUTH.TABLE", id));
-
-        String basicAuth = "";
         try {
-            basicAuth = apiAuthRec.getBasicAuth().getValue();
-            myJwtToken = apiAuthRec.getJwtToken().getValue();
+            CustomerRecord recordForCustomer = new CustomerRecord(currentRecord);
+
+            String id = "";
+            id = "AML";
+            DataAccess da = new DataAccess(this);
+            EbJblApiAuthTableRecord apiAuthRec = new EbJblApiAuthTableRecord(da.getRecord("EB.JBL.API.AUTH.TABLE", id));
+
+            String basicAuth = "";
+            try {
+                basicAuth = apiAuthRec.getBasicAuth().getValue();
+                myJwtToken = apiAuthRec.getJwtToken().getValue();
+            } catch (Exception e1) {
+            }
+
+            encryptedBase64Credentials = basicAuth;
+
+            // Check either JWT is generated or not/EXPIRED.
+            if (myJwtToken == "") {
+                // GET API Call to Generate JWT Auth Token
+                String GET_URL_TP = "";
+                GET_URL_TP = "http://localhost:9089/irf-auth-token-generation-container-21.0.59/api/v1.0.0/generateauthtoken";
+                StringBuilder jwtResponse = null;
+                jwtResponse = this.makeGetRequestForJWT(GET_URL_TP);
+
+                try {
+                    String trimmedResponse = jwtResponse.toString().replaceAll("^\"|\"$", "").replace("\\", "");
+                    JSONObject jsonResponse = new JSONObject(trimmedResponse);
+
+                    // Extract the token value
+                    String token = "";
+                    if (jsonResponse.has("id_token")) {
+                        token = jsonResponse.getString("id_token");
+                    } else {
+                        System.out.println("Error: id_token is not found in JSON response");
+                    }
+
+                    // Tracer
+                    try (FileWriter fw = new FileWriter("/Temenos/T24/UD/Tracer/JwtToken-" + currentRecordId + ".txt",
+                            true); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
+                        out.println(" token: " + token + "\n");
+                    } catch (IOException e) {
+                    }
+                    // Tracer END
+
+                    // Write the JWT Token in EB.JBL.API.AUTH.TABLE Template...
+                    EbJblApiAuthTableTable apiAuthTable = new EbJblApiAuthTableTable(this);
+                    apiAuthRec.setJwtToken(token);
+
+                    try {
+                        apiAuthTable.write(id, apiAuthRec);
+                    } catch (T24IOException e) {
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error occurred while extracting token: " + e.getMessage());
+                }
+                // JWT Token Done
+            }
+
+            // Calling the MAIN API - GET Method - makeGetRequestForAML
+            String GET_URL_TP = "";
+            GET_URL_TP = "http://localhost:9089/CusJwtContainer/api/v1.0.0/party/ws/800155";
+
+            StringBuilder amlResponse = null;
+            try {
+                amlResponse = this.makeGetRequestForAML(GET_URL_TP);
+            } catch (NullPointerException e) {
+            }
+            /*
+             * try { // Sleep for 6 seconds (6000 milliseconds)
+             * Thread.sleep(6000); } catch (InterruptedException e) { // Handle
+             * interrupted exception if necessary e.printStackTrace(); }
+             */
+
+            // Tracer
+            try (FileWriter fw = new FileWriter("/Temenos/T24/UD/Tracer/AML-" + currentRecordId + ".txt", true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    PrintWriter out = new PrintWriter(bw)) {
+                out.println("AmlResponse: " + amlResponse + "\n");
+            } catch (IOException e) {
+            }
+            // Tracer END
+
+            JSONObject jsonAml = null;
+            try {
+                jsonAml = new JSONObject(amlResponse.toString());
+                if (jsonAml.getJSONObject("header").get("status").toString().equals("success")) {
+                    recordForCustomer.setAmlCheck("SENT");
+                }
+
+                else {
+                    // EMPTY the EXPIRED JWT Token in EB.JBL.API.AUTH.TABLE
+                    // Template...
+                    String token = "";
+                    EbJblApiAuthTableTable apiAuthTable = new EbJblApiAuthTableTable(this);
+                    apiAuthRec.setJwtToken(token);
+
+                    try {
+                        apiAuthTable.write(id, apiAuthRec);
+                    } catch (T24IOException e) {
+                    }
+                }
+
+            } catch (JSONException e) {
+            }
+            currentRecord.set(recordForCustomer.toStructure());
         } catch (Exception e1) {
         }
-
-        encryptedBase64Credentials = basicAuth;
-
-        // Check either JWT is generated or not/EXPIRED.
-        if (myJwtToken == "") {
-            // GET API Call to Generate JWT Auth Token
-            String GET_URL_TP = "";
-            GET_URL_TP = "http://localhost:9089/irf-auth-token-generation-container-21.0.59/api/v1.0.0/generateauthtoken";
-            StringBuilder jwtResponse = null;
-            jwtResponse = this.makeGetRequestForJWT(GET_URL_TP);
-
-            try {
-                String trimmedResponse = jwtResponse.toString().replaceAll("^\"|\"$", "").replace("\\", "");
-                JSONObject jsonResponse = new JSONObject(trimmedResponse);
-
-                // Extract the token value
-                String token = "";
-                if (jsonResponse.has("id_token")) {
-                    token = jsonResponse.getString("id_token");
-                } else {
-                    System.out.println("Error: id_token is not found in JSON response");
-                }
-
-                // Tracer
-                try (FileWriter fw = new FileWriter("/Temenos/T24/UD/Tracer/DECRYPT-" + currentRecordId + ".txt", true);
-                        BufferedWriter bw = new BufferedWriter(fw);
-                        PrintWriter out = new PrintWriter(bw)) {
-                    out.println(" token: " + token + "\n");
-                } catch (IOException e) {
-                }
-                // Tracer END
-
-                // Write the JWT Token in EB.JBL.API.AUTH.TABLE Template...
-                EbJblApiAuthTableTable apiAuthTable = new EbJblApiAuthTableTable(this);
-                apiAuthRec.setJwtToken(token);
-
-                try {
-                    apiAuthTable.write(id, apiAuthRec);
-                } catch (T24IOException e) {
-                }
-            } catch (Exception e) {
-                System.out.println("Error occurred while extracting token: " + e.getMessage());
-            }
-            // JWT Token Done
-        }
-
-        // Calling the MAIN API - GET Method - makeGetRequestForAML
-        String GET_URL_TP = "";
-        GET_URL_TP = "http://localhost:9089/CusJwtContainer/api/v1.0.0/party/ws/800155";
-        StringBuilder amlResponse = null;
-        amlResponse = this.makeGetRequestForAML(GET_URL_TP);
-        
-        try {
-            // Sleep for 6 seconds (6000 milliseconds)
-            Thread.sleep(6000);
-        } catch (InterruptedException e) {
-            // Handle interrupted exception if necessary
-            e.printStackTrace();
-        }
-
-        // Tracer
-        try (FileWriter fw = new FileWriter("/Temenos/T24/UD/Tracer/DECRYPT-" + currentRecordId + ".txt", true);
-                BufferedWriter bw = new BufferedWriter(fw);
-                PrintWriter out = new PrintWriter(bw)) {
-            out.println("AmlResponse: " + amlResponse + "\n");
-        } catch (IOException e) {
-        }
-        // Tracer END
-
-        JSONObject jsonAml = null;
-        try {
-            jsonAml = new JSONObject(amlResponse.toString());
-        } catch (JSONException e) {
-        }
-
-        try {
-            if (jsonAml.getJSONObject("header").get("status").toString().equals("success")) {
-                recordForCustomer.setAmlCheck("SENT");
-            }
-
-            else {
-                // EMPTY the EXPIRED JWT Token in EB.JBL.API.AUTH.TABLE
-                // Template...
-                String token = "";
-                EbJblApiAuthTableTable apiAuthTable = new EbJblApiAuthTableTable(this);
-                apiAuthRec.setJwtToken(token);
-
-                try {
-                    apiAuthTable.write(id, apiAuthRec);
-                } catch (T24IOException e) {
-                }
-            }
-
-        } catch (JSONException e) {
-        }
-        currentRecord.set(recordForCustomer.toStructure());
     }
     // END of MAIN Method
 
@@ -241,7 +239,7 @@ public class GbJblICusAML extends RecordLifecycle {
             con.setRequestProperty("Authorization", myJwtToken);
             con.setDoOutput(true);
             con.setConnectTimeout(5000); // Set connection timeout to 5 seconds
-            con.setReadTimeout(5000); // Set read timeout to 5 seconds
+            con.setReadTimeout(10000); // Set read timeout to 10 seconds
 
             int responseCode = con.getResponseCode();
             try {
